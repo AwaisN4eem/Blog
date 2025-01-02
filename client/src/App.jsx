cf79b2a0ab9db88f91ecf650d5b5c46fc021d6fc
ahmadali883 <khahmadalikhan@gmail.com>
2025-01-02 08:14:47 +0500

    setting up ahmad branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 
 import {Route, Routes} from 'react-router-dom'
-import Home from "./pages/P_Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import PostDetails from './pages/PostDetails'
 import CreatePost from './pages/CreatePost'
 import EditPost from './pages/EditPost'
 import Profile from './pages/Profile'
-import {  UserContextProvider } from './context/UserContext'
 import MyBlogs from './pages/MyBlogs'
+
+
+import Home from "./pages/P_Home"
+import {  UserContextProvider } from './context/UserContext'
 import ContestCard from '../src/pages/ContestCard'
 import Blogs from './pages/Blogs'
 import LoginPage from './pages/LoginPage'
@@ -26,19 +28,18 @@ const App = () => {
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/contest" element={<ContestCard/>}/>
       <Route exact path="/blogs" element={<Blogs/>}/>
-      <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/submittable" element={<Submittable/>}/>
       <Route exact path="/signup" element={<SignUp/>}/>
       <Route exact path="/guidelines" element={<GuidelineAndSubmission/>}/>
       <Route exact path="/blog" element={<Blog/>}/>
-      {/* <Route exact path="/createpost" element={<CreatePost/>}/> */}
-
-      
+      <Route exact path="/login" element={<LoginPage/>}/>
 
 
-      {/* <Route exact path="/login" element={<Login/>}/>
+      {/* previous version */}
+      {/* <Route exact path="/createpost" element={<CreatePost/>}/> 
+      <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
-      <Route exact path="/write" element={<CreatePost/>}/>
+      <Route exact path="/write" element={<CreatePost/>}/>      
       <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
       <Route exact path="/edit/:id" element={<EditPost/>}/>
       <Route exact path="/myblogs/:id" element={<MyBlogs/>}/>
